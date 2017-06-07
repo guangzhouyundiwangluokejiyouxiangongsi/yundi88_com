9f954ffe4ab5bafa665f980d52bde072c9bf0f4d
a208349@163.com <a208349@163.com>
2017-06-07 21:04:56 +0800

    添加后台查看h5留言

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -431,7 +431,11 @@ class UserController extends BaseController {
     // 用户留言
     public function message()
     {   
-        $message = M('messageboard')->select();
+        $count      = M('messageboard')->count();// 查询满足要求的总记录数
+        $Page       = new \Think\Page($count,15);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $show       = $Page->show();// 分页显示输出
+        $message = M('messageboard')->order('addtime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $this->assign('page',$show);
         $this->assign('message',$message);
         $this->display();
     }
@@ -539,6 +543,18 @@ class UserController extends BaseController {
             }
         }
         $this->assign('lunbo',$tiyan_lunbo);
+        $this->display();
+    }
+
+    // 用户留言
+    public function h5message()
+    {   
+        $count      = M('user_contact')->count();// 查询满足要求的总记录数
+        $Page       = new \Think\Page($count,15);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $show       = $Page->show();// 分页显示输出
+        $message = M('user_contact')->order('addtime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $this->assign('page',$show);
+        $this->assign('message',$message);
         $this->display();
     }
 }
