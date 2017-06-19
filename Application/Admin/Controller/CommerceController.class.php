949c86e4044f34cd09e5956564a2fbcc572fada9
a208349@163.com <a208349@163.com>
2017-06-19 19:48:34 +0800

    添加公告内容

--- Application/Admin/Controller/CommerceController.class.php
+++ Application/Admin/Controller/CommerceController.class.php
@@ -21,13 +21,15 @@
 
 		// 添加公告
 		public function notice_add()
-		{
+		{	
+			$this->initEditor();
 			$this->display();
 		}
 
 		public function addData()
 		{
 			$data['content'] = I('post.content');
+			$data['text'] = I('post.text');
 			$data['addtime'] = time();
 			$res = M('notice')->data($data)->add();
 			if ($res) {
@@ -42,12 +44,9 @@
 		{
             if(IS_POST){
                 $cont = M('notice')->field('content')->where(array('id'=>I('post.id')))->find();
-                if ($cont['content'] == I('post.content')) {
-                    $this->success('修改成功', 'notice');
-                    exit;
-                }
                 $data['content'] = I('post.content');
-                $result = M('notice')->where(array('id'=>I('post.id')))->data($data)->save();
+                $data['text'] = I('post.text');
+                $result = M('notice')->where(array('id'=>I('post.id')))->save($data);
                 if($result){
                     $this->success('修改成功', 'notice');exit;
                 }else{
@@ -59,6 +58,7 @@
             $res = M('notice')->field('id,content')->where(array('id'=>$id))->find();
             $this->assign('id', $id);
             $this->assign('res', $res);
+            $this->initEditor();
 	        $this->display();
 		}
 		// 删除公告
@@ -72,6 +72,23 @@
                 $this->error('删除失败','notice');
             }
 		}
+
+
+		/**
+	     * 初始化编辑器链接
+	     * @param $post_id post_id
+	     */
+	    private function initEditor()
+	    {
+	        $this->assign("URL_upload", U('Admin/Ueditor/imageUp',array('savepath'=>'article')));
+	        $this->assign("URL_fileUp", U('Admin/Ueditor/fileUp',array('savepath'=>'article')));
+	        $this->assign("URL_scrawlUp", U('Admin/Ueditor/scrawlUp',array('savepath'=>'article')));
+	        $this->assign("URL_getRemoteImage", U('Admin/Ueditor/getRemoteImage',array('savepath'=>'article')));
+	        $this->assign("URL_imageManager", U('Admin/Ueditor/imageManager',array('savepath'=>'article')));
+	        $this->assign("URL_imageUp", U('Admin/Ueditor/imageUp',array('savepath'=>'article')));
+	        $this->assign("URL_getMovie", U('Admin/Ueditor/getMovie',array('savepath'=>'article')));
+	        $this->assign("URL_Home", "");
+	    }
 
 
 	}
